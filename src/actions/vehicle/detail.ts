345a7782eb94b91d2029802b5bb975394e1f9a18
Joaocc1 <joaoc86@gmail.comm>
2023-05-13 17:20:35 +0100

    create actions

--- src/actions/vehicle/detail.ts
+++ src/actions/vehicle/detail.ts
@@ -1,3 +1,16 @@
 import { Request, Response } from "express";
+import { detail } from "../../services/vehicle";
 
-export default async (request: Request, response: Response) => {};
+export default async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  const vehicle = await detail(id);
+
+  if (!vehicle) {
+    return response
+      .status(404)
+      .json({ code: 404, message: "Veiculo não encontrado" });
+  }
+
+  return response.json(vehicle);
+};
--- src/actions/vehicle/remove.ts
+++ src/actions/vehicle/remove.ts
@@ -1,3 +1,16 @@
 import { Request, Response } from "express";
+import { detail, remove } from "../../services/vehicle";
 
-export default async (request: Request, response: Response) => {};
+export default async (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  if (!(await detail(id))) {
+    return response.status(404).json({
+      code: 404,
+      message: "Veiculo não encontrado",
+    });
+  }
+
+  await remove(id);
+  return response.json();
+};
